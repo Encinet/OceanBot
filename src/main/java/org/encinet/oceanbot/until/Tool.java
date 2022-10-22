35716e3d02b557da1708de4c3a2b90fcfdde7958
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-10-22 14:29:38 +0000

    fix

--- src/main/java/org/encinet/oceanbot/until/Tool.java
+++ src/main/java/org/encinet/oceanbot/until/Tool.java
@@ -1,7 +1,7 @@
 package org.encinet.oceanbot.until;
 
 public class Tool {
-    public static boolean listCon(List<T> list, T element) {
+    public static <T> boolean listCon(List<T> list, T element) {
         for (T n : list) {
             if (Object.equals(n, element)) {
                 return true;
