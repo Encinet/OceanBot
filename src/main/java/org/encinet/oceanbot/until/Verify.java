2382528c17c8e3c6ecfd5ef354440f98e8f4a9c4
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-10-23 10:05:41 +0800

    Update Verify.java

--- src/main/java/org/encinet/oceanbot/until/Verify.java
+++ src/main/java/org/encinet/oceanbot/until/Verify.java
@@ -3,36 +3,17 @@ package org.encinet.oceanbot.until;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Verify {
-    private static StringBuilder verify;
     private static final int extent = 7;
-
-    /**
-     * 密码中的所有类型
-     */
-    private static final String[] TYPE = {
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-            "abcdefghijklmnopqrstuvwxyz",
-            "0123456789"
-    };
 
     public static String get() {
         return generate();
     }
 
     private static String generate() {
-        verify = new StringBuilder(extent);
+        StringBuilder verify = new StringBuilder(extent);
         for (byte i = 0; i < extent; i++) {
-            g(ThreadLocalRandom.current().nextInt(3));
+            verify.append(ThreadLocalRandom.current().nextInt(9));
         }
         return verify.toString();
-    }
-
-    private static void g(int options) {
-        int randomInt = ThreadLocalRandom.current().nextInt(switch (options) {
-            case 0, 1 -> 26;
-            case 2 -> 10;
-            default -> 0;
-        });
-        verify.append(TYPE[options].charAt(randomInt));
     }
 }
