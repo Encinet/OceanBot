6336777c928312ef2a70dada7501bb7f4a619fba
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-11-11 16:07:55 +0000

    安全

--- src/main/java/org/encinet/oceanbot/event/PlayerLogin.java
+++ src/main/java/org/encinet/oceanbot/event/PlayerLogin.java
@@ -2,6 +2,7 @@ package org.encinet.oceanbot.event;
 
 import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.api.MiraiMC;
+import me.dreamvoid.miraimc.api.bot.MiraiGroup;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
@@ -14,12 +15,20 @@ import static org.encinet.oceanbot.Config.*;
 import static org.encinet.oceanbot.QQ.Bind.code;
 
 public class PlayerLogin implements Listener {
+    private MiraiGroup members;
+
     @EventHandler
     public void onPlayerJoin(AsyncPlayerPreLoginEvent e) {
         boolean allow = false;
         long binder = MiraiMC.getBind(e.getUniqueId());
         if (binder != 0) {
-            if (MiraiBot.getBot(BotID).getGroup(MainGroup).contains(binder)) {
+            members = MiraiBot.getBot(BotID).getGroup(MainGroup);
+
+            if (members != null) {
+                e.disallow(
+                        AsyncPlayerPreLoginEvent.Result.KICK_OTHER,
+                        LegacyComponentSerializer.legacyAmpersand().deserialize("服务器启动中, 请稍后再尝试进入"));
+            } else if (members.contains(binder)) {
                 allow = true;
             }
         }
@@ -36,8 +45,8 @@ public class PlayerLogin implements Listener {
             String message = noWhiteKick.replace("%verify%", verify);
             e.disallow(
                     AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST,
-                    LegacyComponentSerializer.legacyAmpersand().deserialize(ChatColor.translateAlternateColorCodes('&', message))
-            );
+                    LegacyComponentSerializer.legacyAmpersand()
+                            .deserialize(ChatColor.translateAlternateColorCodes('&', message)));
         }
     }
 }
