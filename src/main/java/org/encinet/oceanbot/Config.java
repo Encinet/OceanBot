2ef030d191b4dbb8a88589c256d166dcbb4449aa
ObcbO <49872497+ObcbO@users.noreply.github.com>
2022-11-12 15:49:21 +0000

    fix

--- src/main/java/org/encinet/oceanbot/Config.java
+++ src/main/java/org/encinet/oceanbot/Config.java
@@ -3,15 +3,16 @@ package org.encinet.oceanbot;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Config {
     static final Plugin plugin = JavaPlugin.getProvidingPlugin(OceanBot.class);
-    private static final @NotNull FileConfiguration config = plugin.getConfig();
+    
+    public static FileConfiguration config() {
+        return plugin.getConfig();
+    }
 
     public static int ver;
     public static List<String> prefix;
@@ -35,14 +36,14 @@ public class Config {
     public static void load() {
         plugin.reloadConfig();
 
-        ver = config.getInt("ver", 3);
-        prefix = config.getStringList("prefix");
-        BotID = config.getLong("BotID");
-        GroupID = config.getLongList("GroupID");
-        MainGroup = config.getLong("MainGroup");
+        ver = config().getInt("ver", 3);
+        prefix = config().getStringList("prefix");
+        BotID = config().getLong("BotID");
+        GroupID = config().getLongList("GroupID");
+        MainGroup = config().getLong("MainGroup");
 
         numMessage = new HashMap<>();
-        List<Map<?, ?>> nums = config.getMapList("NumMessage");
+        List<Map<?, ?>> nums = config().getMapList("NumMessage");
         System.out.println(nums);
         for (final Map<?, ?> map : nums) {
             int num = (int) map.get("num");
@@ -52,16 +53,16 @@ public class Config {
             System.out.println(num + " " + messages);
         }
 
-        noWhiteKick = config.getString("noWhiteKick");
-        join = config.getString("join");
-        chatPrefix = config.getStringList("chat.prefix");
-        serverToQQ = config.getString("chat.format.server-to-qq");
+        noWhiteKick = config().getString("noWhiteKick");
+        join = config().getString("join");
+        chatPrefix = config().getStringList("chat.prefix");
+        serverToQQ = config().getString("chat.format.server-to-qq");
 
-        admin = config.getLongList("admin");
+        admin = config().getLongList("admin");
         
-        recallEnable = config.getBoolean("recall.enable", true);
-        recallMuteValue = config.getInt("recall.mute.value", 3);
-        recallMuteTime = config.getInt("recall.mute.time", 120);
-        recallText = config.getStringList("recall.text");
+        recallEnable = config().getBoolean("recall.enable", true);
+        recallMuteValue = config().getInt("recall.mute.value", 3);
+        recallMuteTime = config().getInt("recall.mute.time", 120);
+        recallText = config().getStringList("recall.text");
     }
 }
