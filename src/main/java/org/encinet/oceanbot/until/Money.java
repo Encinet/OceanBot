bd16d33669bc7e3b906eab24a5cd186aaff177ad
ObcbO <m17089494917@163.com>
2022-12-22 13:21:25 +0800

    fix

--- src/main/java/org/encinet/oceanbot/until/Money.java
+++ src/main/java/org/encinet/oceanbot/until/Money.java
@@ -47,7 +47,7 @@ public class Money {
     public static double trueMoney(OfflinePlayer player, double change) {
         double playerHave = OceanBot.econ.getBalance(player);
         // playerHave do not smaller than 0
-        return playerHave > -change ? change : -playerHave;
+        return playerHave < -change ? change : -playerHave;
     }
 
     public static Map<String, Integer> getTodayMap() {
