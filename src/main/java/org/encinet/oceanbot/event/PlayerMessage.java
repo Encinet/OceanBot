95d7b47a4e75b69ccc7e09440e8855e5f0f4a55e
ObcbO <m17089494917@163.com>
2022-12-22 21:22:45 +0800

    Server to QQ @功能

--- src/main/java/org/encinet/oceanbot/event/PlayerMessage.java
+++ src/main/java/org/encinet/oceanbot/event/PlayerMessage.java
@@ -3,9 +3,15 @@ package org.encinet.oceanbot.event;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.dreamvoid.miraimc.api.MiraiBot;
+import me.dreamvoid.miraimc.api.MiraiMC;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Collection;
+import java.util.UUID;
 
 import static org.encinet.oceanbot.Config.*;
 
@@ -13,15 +19,37 @@ public class PlayerMessage implements Listener {
 
     @EventHandler
     public void playerMessage(AsyncChatEvent e) {
-        String message = PlainTextComponentSerializer.plainText().serialize(e.message());
+        String get = PlainTextComponentSerializer.plainText().serialize(e.message());
         for (String n : chatPrefix) {
-            if (message.startsWith(n)) {
+            if (get.startsWith(n)) {
+                String message = atPlayer(get.substring(1));
                 String formatText = serverToQQ
                         .replace("$[player]", e.getPlayer().getName())
-                        .replace("$[message]", message.substring(1));
+                        .replace("$[message]", message);
                 MiraiBot.getBot(BotID).getGroup(MainGroup)
                         .sendMessageMirai(PlaceholderAPI.setPlaceholders(e.getPlayer(), formatText));
+                return;
             }
         }
+    }
+
+    /**
+     * At字符串替换
+     *
+     * @param text 消息
+     * @return 格式化后消息
+     */
+    private String atPlayer(String text) {
+        Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+        for (Player player : players) {
+            if (text.toLowerCase().contains(player.getName().toLowerCase())) {
+                UUID uuid = player.getUniqueId();
+                long qqNum = MiraiMC.getBind(uuid);
+
+                text = text.replace("@" + player.getName(), "[mirai:at:" + qqNum + "]");
+                text = text.replace(player.getName(), "[mirai:at:" + qqNum + "]");
+            }
+        }
+        return text;
     }
 }
