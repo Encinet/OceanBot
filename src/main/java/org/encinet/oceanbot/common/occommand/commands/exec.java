1d816f49430de332c9c3ac28bfcec559f0ae9ac2
ObcbO <m17089494917@163.com>
2023-03-17 00:53:15 +0800

    fix

--- src/main/java/org/encinet/oceanbot/common/occommand/commands/exec.java
+++ src/main/java/org/encinet/oceanbot/common/occommand/commands/exec.java
@@ -2,20 +2,20 @@ package org.encinet.oceanbot.common.occommand.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
-import org.bukkit.command.MessageCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.encinet.oceanbot.OceanBot;
 import org.encinet.oceanbot.common.occommand.BasicCommand;
 import org.encinet.oceanbot.file.Config;
+import org.encinet.oceanbot.until.QQCommandSender;
 import org.encinet.oceanbot.until.TextUntil;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class exec extends BasicCommand implements MessageCommandSender {
-  private StringBuilder print = new StringBuilder();
+public class exec extends BasicCommand {
 
   public exec() {
     super("exec", new String[] {"c", "执行"}, "<命令>", "执行控制台命令", true);
@@ -28,8 +28,7 @@ public class exec extends BasicCommand implements MessageCommandSender {
       return "无法找到需要执行的命令";
     } else {
       String cmd = split[1];
-      print = new StringBuilder();
-      MessageCommandSender sender = this;
+      QQCommandSender sender = new QQCommandSender(Bukkit.getConsoleSender());
       new BukkitRunnable() {
         @Override
         public void run() {
@@ -41,7 +40,13 @@ public class exec extends BasicCommand implements MessageCommandSender {
           OceanBot.core
               .getBot()
               .getGroup(Config.LogGroup)
-              .sendMessage("来自" + qq + "的命令 " + cmd + "\n---\n" + TextUntil.removeColorCodes(print.toString().trim()));
+              .sendMessage(
+                  "来自"
+                      + qq
+                      + "的命令 "
+                      + cmd
+                      + "\n---\n"
+                      + TextUntil.removeColorCodes(sender.get()));
         }
       }.runTask(OceanBot.plugin);
       return "指令发送完成";
@@ -51,32 +56,5 @@ public class exec extends BasicCommand implements MessageCommandSender {
   @Override
   public String onTab(String[] args, long qq) {
     return null;
-  }
-
-  @Override
-  public void sendMessage(String message) {
-    print.append(message).append("\n");
-  }
-
-  @Override
-  public void sendMessage(String... messages) {
-    for (String message : messages) {
-      print.append(message).append("\n");
-    }
-  }
-
-  @Override
-  public void sendMessage(UUID sender, String message) {
-      this.sendMessage(message);
-  }
-
-  @Override
-  public void sendMessage(UUID sender, String... messages) {
-      this.sendMessage(messages);
-  }
-
-  @Override
-  public String getName() {
-    return super.getHead();
   }
 }
